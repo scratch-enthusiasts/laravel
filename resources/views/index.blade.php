b207f217facf79fa743a0438ffca38d036197a90
FriendlyVideogamePlayer <zakariahahah@gmail.com>
2021-07-18 12:16:17 +0100

    Add simple homepage
    Added a simple homepage and set up a css file

---
+++ resources/views/index.blade.php
@@ -0,0 +1,20 @@
+<html lang="en">
+@include('components.head')
+<body>
+<div role="main">
+    <div class="container-fluid text-center justify-content-center">
+        <div class="index-center">
+            <h1 class="justify-content-center pb-5 pt-5" role="heading" aria-level="1">
+                Music player
+            </h1>
+            <div class="index">
+                <p class="lead text-card pb-3">Using music player you can collaborate on your playlists and allow your friends to control your music with you</p>
+                <a href="" class="btn btn-primary my-3">Connect your Spotify!</a>
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
