85afa155cc00ab28fe28886cc597f26709fcd0ac
FriendlyVideogamePlayer <zakariahahah@gmail.com>
2021-07-19 14:11:59 +0100

    Add socket_clients migration

---
+++ database/migrations/2021_07_19_124747_create_socket_clients_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSocketClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('socket_clients', function (Blueprint $table) {
+            $table->increments('client_id');
+            $table->string('client_inet', 21)->unique();
+            $table->string('client_nickname', 32)->unique();
+            $table->timestamp('time_connected')->useCurrent();
+            $table->timestamp('time_lastaction')->useCurrent();
+            $table->boolean('flag_disconnect')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('socket_clients');
+    }
+}
